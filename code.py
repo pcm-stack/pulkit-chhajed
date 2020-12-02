93c3d33a15f3499954b03414c5d2a11dd118f6d1
pcm-stack <75384418+pcm-stack@users.noreply.github.com>
2020-12-02 22:19:53 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,5 @@
+i=input("Enter the file name with the extension:")
+if i.endswith('.py'):
+    print(i,"=Python")
+else:
+    print("it is something else")
---
+++ list.py
@@ -0,0 +1,3 @@
+i=int(input("Enter the radius of the circle :"))
+i=22/7*i*i
+print("The area of the circle is:",i)
